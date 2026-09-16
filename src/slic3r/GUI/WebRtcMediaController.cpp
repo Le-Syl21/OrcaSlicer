2c2e91b6293b46e67fec37b821d34a5cca34af9f
Ian Chua <iancrb00@gmail.com>
2026-09-16 14:36:49 +0800

    fix: remove hardcoded ICE servers

--- src/slic3r/GUI/WebRtcMediaController.cpp
+++ src/slic3r/GUI/WebRtcMediaController.cpp
@@ -181,8 +181,6 @@ void WebRtcMediaController::on_ready(std::vector<CameraIceServer> servers)
             BOOST_LOG_TRIVIAL(warning) << "WebRTC: invalid ICE server: " << e.what();
         }
     }
-    configuration.iceServers.emplace_back("stun:stun.cloudflare.com:3478");
-    configuration.iceServers.emplace_back("stun:stun.l.google.com:19302");
 
     BOOST_LOG_TRIVIAL(info) << "WebRTC: creating peer connection with " << configuration.iceServers.size() << " ice servers";
     auto peer_connection = std::make_shared<rtc::PeerConnection>(std::move(configuration));
